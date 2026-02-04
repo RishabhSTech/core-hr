7331975b14f1da47b7f05630b940ebcbf012419e
Rishabh Pratap <rishabh.pratap@arnasoftech.com>
2026-02-04 22:13:01 +0530

    404 found fix

--- supabase/migrations/20251214160740_67531854-105c-4e87-9058-9ed86485b377.sql
+++
@@ -1,5 +0,0 @@
--- Allow anyone to search for active companies (only basic info exposed)
-CREATE POLICY "Anyone can search active companies"
-ON public.companies
-FOR SELECT
-USING (is_active = true);
